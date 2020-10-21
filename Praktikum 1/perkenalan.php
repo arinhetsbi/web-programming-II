503ec74814f7baad37303d08cfce9eb43548ee9b
arinhetsbi <you@example.com>
2020-10-22 01:08:36 +0700

    wp 2

---
+++ Praktikum 1/perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class perkenalan extends CI_Controller
+{
+    public function index()
+    {
+    echo "<h1> Perkenalan</h1>";
+    echo "Nama saya Arin
+    Saya tinggal di Tegal
+    saya suka apa aja yg bikin seneng";
+    }
+}
